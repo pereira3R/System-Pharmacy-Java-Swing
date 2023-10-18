81112ed57fca719d4ad9e3024fd775796f6faa39
pereira3R <anthony_rodriguespereira@outlook.com>
2023-10-18 16:51:42 -0400

    start

---
+++ system/src/main/java/com/mycompany/system/System.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.system;
+
+public class System {
+
+    public static void main(String[] args) {
+    }
+
+}
